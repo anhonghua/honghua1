797f7bdc67b6c2aadf93a9ecd33c5968b934de6f
1343406667@qq.com <ThinkPad@DESKTOP-FTPNDT1>
2018-07-20 12:45:35 +0800

    7-19

--- JavaClass_0712/src/edu/yust/ceceom/split.java
+++ JavaClass_0712/src/edu/yust/ceceom/split.java
@@ -9,7 +9,7 @@ public class split {
 
 	public static void main(String[] args) throws Exception {
 	
-		File file = new File("C:/Users/ThinkPad/Desktop/order.txt");
+		File file = new File("C:/Users/ThinkPad/Desktop/11.xls");
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		String s = null;
 		while ((s = br.readLine()) != null) {
